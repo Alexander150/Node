931b8b7e5cbd421a4ab65aac458aae87c3834006
zarnik119 <zarnik119@gmail.com>
2019-04-24 15:39:56 +0300

    x

---
+++ db/migrate/20190424123922_create_data_types.rb
@@ -0,0 +1,10 @@
+class CreateDataTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :data_types do |t|
+      t.string :name
+      t.string :class_name
+
+      t.timestamps
+    end
+  end
+end
