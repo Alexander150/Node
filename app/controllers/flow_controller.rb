dbe62250e64f189ffe5f984216134f7ce1e31f3a
zarnik119 <zarnik119@gmail.com>
2019-05-05 00:10:23 +0300

    Работает доюавление метрик в node

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -6,14 +6,14 @@ class FlowController < ApplicationController
 
   	@first_node = Node.find_by :first_node => true
   	@last_node = Node.find_by :last_node => true
-  	@current_node = @first_node
-  	while (!@current_node.nil?)
-      @edges+=[@current_node.edges[0]]
+  	current_node = @first_node
+  	while (!current_node.nil?)
+      @edges+=[current_node.edges[0]]
 
-      if (@current_node.edges[0] != nil)
-       	@current_node = Node.find_by id: @current_node.edges[0].target_node_id
+      if (current_node.edges[0] != nil)
+       	current_node = Node.find_by id: current_node.edges[0].target_node_id
       else
-       	@current_node = nil
+       	current_node = nil
       end
   	end
   end
@@ -21,16 +21,17 @@ class FlowController < ApplicationController
   def edges
     @edges = []
 
+    @metrics = Metric.all
     @first_node = Node.find_by :first_node => true
     @last_node = Node.find_by :last_node => true
-    @current_node = @first_node
-    while (!@current_node.nil?)
-      @edges+=[@current_node.edges[0]]
+    current_node = @first_node
+    while (!current_node.nil?)
+      @edges+=[current_node.edges[0]]
        
-      if (@current_node.edges[0] != nil)
-        @current_node = Node.find_by id: @current_node.edges[0].target_node_id
+      if (current_node.edges[0] != nil)
+        current_node = Node.find_by id: current_node.edges[0].target_node_id
       else
-        @current_node = nil
+        current_node = nil
       end
     end
 
